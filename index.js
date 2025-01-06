a49544e18a69f3bce4d4615eb37be8f3d938ca5e
Khosbayar707 <torutora0415@gmail.com>
2025-01-06 18:37:52 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const fs = require("node:fs");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// read file
+app.get("/movies", (req, res) => {
+  const data = fs.readFileSync("data/movies.json", "utf8");
+  const movies = JSON.parse(data);
+  res.json(movies);
+});
+
+// add new obj to json
+app.get("/movies/create", (req, res) => {
+  const { name } = req.query;
+  // 1. read file
+  const data = fs.readFileSync("data/movies.json", "utf8");
+  const movies = JSON.parse(data);
+  // 2. push to json
+  movies.push({ id: Date.now(), name });
+  // 3. write json file
+  const moviesString = JSON.stringify(movies, null, 4);
+  fs.writeFileSync("data/movies.json", moviesString);
+
+  res.json({ messege: "Success" });
+});
+
+// update obj of json
+app.get("/movies/update", (req, res) => {
+  const { name } = req.query;
+  // 1. read file
+  const data = fs.readFileSync("data/movies.json", "utf8");
+  const movies = JSON.parse(data);
+  // 2. find the item
+  movies.find((id = 1736158100990), name);
+  // 3. update the json arrey
+  const moviesString = JSON.stringify(movies, null, 4);
+  fs.writeFileSync("data/movies.json", moviesString);
+
+  res.json({ messege: "Updated" });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
