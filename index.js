b1ba1836a3e9949ee242158c44b412dfa5170308
Khosbayar707 <torutora0415@gmail.com>
2025-01-08 18:15:49 +0800

    1

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const fs = require("node:fs");
 const app = express();
-const port = 3000;
+const port = 4000;
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 const findAllMovies = () => {
   const data = fs.readFileSync("data/movies.json", "utf8");
-  const movies = JSON.parse(data);
+  return JSON.parse(data);
 };
+
+app.get("/", (req, res) => {
+  const content = findAllMovies();
+  res.send(content);
+});
 
 //1. find movie by id
 
-const findById = () => {
+const findById = (req, res) => {
   const { id } = req.params;
   const data = fs.readFileSync("data/movies.json", "utf8");
   const movies = JSON.parse(data);
@@ -28,34 +32,43 @@ app.get("/movie/:id", findById);
 
 //2. delete move from arrey
 
-app.delete("/movie/:id", (req, res) => {
-  const movieId = Number(req.params.id);
+const deleteMovie = (req, res) => {
+  const movieId = req.params.id;
   const movies = findAllMovies();
 
-  const updatedMovies = movies.filter((movie) => movie.id !== movieId);
+  console.log(movieId);
+
+  const updatedMovies = movies.filter((movie) => movie.id !== Number(movieId));
   const movieString = JSON.stringify(updatedMovies, null, 4);
   fs.writeFileSync("data/movies.json", movieString);
   res.json({ messege: "Deleted" });
-});
-
+};
+app.delete("/delete/:id", deleteMovie);
 //3. add new movie to arrey
 
-const createNewMovie = () => {
-  const { name } = req.params;
-  const data = fs.readFileSync("data/movies.json", "utf8");
-  const movies = JSON.parse(data);
-  movies.push({ id: Date.now(), name });
+const createNewMovie = (req, res) => {
+  const body = req.body;
+  const movies = findAllMovies();
+  movies.push({ id: Date.now(), ...body });
   const moviesString = JSON.stringify(movies, null, 4);
   fs.writeFileSync("data/movies.json", moviesString);
-  console.log(name);
-  res.json({ messege: "Success" });
+  // console.log(body);
+  res.json(movies);
 };
 
-app.post("/movie/create/:name", createNewMovie);
+app.post("/create", createNewMovie);
 
 //4. update movie data
+const updateMovie = (req, res) => {
+  const body = req.body;
+  const data = fs.readFileSync("data/movies.json", "utf8");
+  const movies = JSON.parse(data);
 
-app.put("/movie/update/:name");
+  const moviesString = JSON.stringify(movies, null, 4);
+  fs.writeFileSync("data/movies.json", moviesString);
+  res.json({ messege: "Updated" });
+};
+app.put("/update", updateMovie);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
